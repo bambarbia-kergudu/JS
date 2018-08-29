2cb6e39c55378795824c4a8af7769296a922af72
bambarbia-kergudu <xseawolf@gmail.com>
2018-08-30 00:37:17 +0300

    Изменение способа получения ключей и значений из localStorage

--- misc/stickynotes/stickynotes.js
+++ misc/stickynotes/stickynotes.js
@@ -6,12 +6,12 @@ function init(){
 	var button = document.getElementById("add_button"); // извлекаем объект кнопки с id = add_button
 	button.onclick = createSticky; // задаем обработчик события и ждем нажатия кнопки
 
-	for (var i = 0; i < localStorage.length; i++){ // подсчитываем кол-во элементов в локальном хранилище
-		var key = localStorage.key(i); // получаем ключ каждого элемента
-		if(key.substring(0, 6) == "sticky"){ // проверяем, начинается ли название ключа со sticky
-			var value = localStorage.getItem(key); // получаем значение по ключу
-			addStickyToDom(value); // передаем значение в функцию
-		}
+	var stickiesArray = getStickiesArray(); // получаем массив ключей для последующей выборки по локальному хранилищу
+
+	for (var i = 0; i < stickiesArray.length; i++) { // подсчитываем кол-во элементов в массиве
+		var key = stickiesArray[i]; // получаем ключ каждого элемента
+		var value = localStorage[key]; // получаем значение по ключу из локального хранилища
+		addStickyToDom(value); // передаем значение в функцию для вывода данных на сайте
 	}
 
 }
@@ -27,8 +27,23 @@ function addStickyToDom(value){
 }
 
 function createSticky(){
+	var stickiesArray = getStickiesArray(); // получаем массив ключей
+	var currentDate = new Date(); // узнаем текущую дату
+	var key = "sticky_" + currentDate.getTime(); // преобразуем текущую дату в кол-во секунд, прошедших с 1970 года и объединяем со sticky_
 	var value = document.getElementById("note_text").value; // извлекаем текст из текстового поля
-	var key = "sticky_" + localStorage.length; // создаем уникальный ключ для заметки
-	localStorage.setItem(key, value); // добавляем новую заметку в локальное хранилище
+	localStorage.setItem(key, value); // добавляем ключ - значение в локальное хранилище
+	stickiesArray.push(key); // добавляем в конец массива новый уникальный ключ для последующей выборки значения по ключу
+	localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray)); // превращаем массив в строку и добавляем в локальное хранилище по ключу stickiesArray
 	addStickyToDom(value); // выводим новую заметку на странице
+}
+
+function getStickiesArray(){
+	var stickiesArray = localStorage.getItem("stickiesArray"); // получаем строку с сохраненными ключами из локального хранилища
+	if(!stickiesArray){ // если строки с сохраненными ключами нет
+		stickiesArray = []; // создаем пустой массив
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray)); // превращаем пустой массив в строку и добавляем в stickiesArray
+	} else { // если строка с сохраненными ключами есть
+		stickiesArray = JSON.parse(stickiesArray); // превращаем строку в массив
+	}
+	return stickiesArray; // возвращаем массив
 }
